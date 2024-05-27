b40ee8fc7ef4c9c688fac7b777c829f9f0f629ef
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-05-27 10:22:03 +0200

    Ejercicio 002

---
+++ src/main/java/com/mycompany/repositorio/de/practicas/Ejercicio002.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.repositorio.de.practicas;
+
+/**
+operadores basicos en java
+ */
+public class Ejercicio002 {
+    public static void main (String [] args){
+        int a = 5;
+        int b = 2;
+        
+        int suma = a + b;
+        int resta = a - b;
+        int multiplicacion = a * b;
+        double division = (double)a / b;
+        int resto = a % b;
+        
+        System.out.println("El resultade de la suma a + b es = " + suma);
+        System.out.println("El resultado de la resta a - b = " + resta);
+        System.out.println("El resultado de la multiplicacion a * b = " + multiplicacion);
+        System.out.println("El resultado de la division a / b = " +  division);
+        System.out.println("El resultado del resto entre la division de a / b = " + resto);
+    } 
+    
+}
