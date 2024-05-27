4ed5f11d19e579efe6e0b4e498db6173aba649d4
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-05-27 09:43:40 +0200

    Ejercicio 001

---
+++ src/main/java/com/mycompany/repositorio/de/practicas/Ejercicio001.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.repositorio.de.practicas;
+
+/**
+Imprime en la consola un Hola mundo
+ */
+public class Ejercicio001 {
+    public static void main (String [] args){
+        System.out.println("Hola Mundo!.");
+    }
+    
+}
