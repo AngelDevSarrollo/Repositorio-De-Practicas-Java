479529ea92d8e3e7e07a2b2c91efde5acb7b52c0
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-05-27 11:29:57 +0200

    Ejercicio 004

---
+++ src/main/java/com/mycompany/repositorio/de/practicas/Ejercicio004.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.repositorio.de.practicas;
+
+/**
+Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los 
+dos. Si son iguales indicarlo también. Ves cambiando los valores para comprobar que 
+funciona.
+ */
+public class Ejercicio004 {
+    public static void main(String[]args){
+        int a = 7;
+        int b = 7;
+        if(a>b){
+            System.out.println("a es mayor que b");
+        }else if(a<b){
+            System.out.println("a es menor que b");
+        }else if(a==b){
+            System.out.println("los dos son iguales");            
+        }
+    }
+    
+}
