488ced5767a4cd78a879f899d950d9fed6e66830
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-05-27 11:37:31 +0200

    Ejercicio 005

---
+++ src/main/java/com/mycompany/repositorio/de/practicas/Ejercicio005.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.repositorio.de.practicas;
+
+/**
+Declara un String que contenga tu nombre, después muestra un mensaje de bienvenida 
+por consola. Por ejemplo: si introduzco «Ángel», me aparezca «Bienvenid@ Ángel».
+ */
+public class Ejercicio005 {
+    public static void main(String[]args){
+    String a = "Bienvenido";
+    String b = "Angel";
+    System.out.println(a+" "+b+"!!!");
+   }
+    
+}
